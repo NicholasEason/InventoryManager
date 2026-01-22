b2e5effbdfd53f96b15af51b69e82134df837e5c
Nicholas Eason <nicholas.eason50@gmail.com>
2026-01-22 16:24:11 -0600

    Added .env support

--- server/index.js
+++ server/index.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 
@@ -10,7 +11,7 @@ app.get("/api/ping", (req, res) => {
   res.send("pong");
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.API_PORT || 5000;
 app.listen(PORT, () => {
   console.log(`API server running on http://localhost:${PORT}`);
 });
